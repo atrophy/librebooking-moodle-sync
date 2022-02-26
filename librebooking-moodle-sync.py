e81a4c5c5a590ea6c14333b4d55c87e653a8318a
Daniel Harmsworth <atrophy@caffeinator.net>
2022-02-26 18:02:42 +1300

    Made it a persistent process

--- librebooking-moodle-sync.py
+++ librebooking-moodle-sync.py
@@ -4,59 +4,65 @@ import configparser
 import untangle
 import requests
 import json
-import pprint
-
-pp = pprint.PrettyPrinter(indent=4)
+import time
 
 config = configparser.ConfigParser(interpolation=None)
 config.read('config/config.ini')
+interval = int(config['schedule']['interval'])
 
-memberships = {}
-stream_mapping = {}
+print("LibreBooking - Moodle Synchroniser Started")
+print("Polling Interval set to: " + str(interval) + " seconds")
+print("------------------------------------------------------------")
 
-credentials = {'username':'automation', 'password': 'E82yEruavpYdUsXhVdwL'}
+while True:
+	memberships = {}
+	stream_mapping = {}
 
-authURI = config['data']['booked_uri'] + "/Authentication/Authenticate"
-r = requests.post(authURI, data=json.dumps(credentials))
-auth = r.json()
+	credentials = {'username':config['booked_credentials']['username'], 'password': config['booked_credentials']['password']}
 
-headers = {'X-Booked-SessionToken':auth['sessionToken'], 'X-Booked-UserId':auth['userId']}
+	authURI = config['data']['booked_uri'] + "/Authentication/Authenticate"
+	r = requests.post(authURI, data=json.dumps(credentials))
+	auth = r.json()
 
-groupsURI = config['data']['booked_uri'] + "/Groups"
-r = requests.get(groupsURI, headers=headers)
+	headers = {'X-Booked-SessionToken':auth['sessionToken'], 'X-Booked-UserId':auth['userId']}
 
-lbGroups = r.json()
+	groupsURI = config['data']['booked_uri'] + "/Groups"
+	r = requests.get(groupsURI, headers=headers)
 
-# Parse the group names to find the mapping for stream Common Module IDs
-for group in lbGroups['groups']:
-	groupName = group['name']
-	if "|" in groupName:
-		stream_id = groupName.split('|')[0].strip()
-		stream_mapping[stream_id] = group['id']
+	lbGroups = r.json()
 
-#gradebook = untangle.parse(config['data']['stream_uri'])
-gradebook = untangle.parse('testdata.xml')
+	# Parse the group names to find the mapping for stream Common Module IDs
+	for group in lbGroups['groups']:
+		groupName = group['name']
+		if "|" in groupName:
+			stream_id = groupName.split('|')[0].strip()
+			stream_mapping[stream_id] = group['id']
 
-memberships = {}
+	#gradebook = untangle.parse(config['data']['stream_uri'])
+	gradebook = untangle.parse('testdata.xml')
 
-for result in gradebook.results.result:
-	if result.score == '100 %':
-		if result.assignment.cdata in stream_mapping:
-			if not result.student.cdata in memberships:
-				memberships[result.student.cdata] = []
+	memberships = {}
 
-			memberships[result.student.cdata].append(int(stream_mapping[result.assignment.cdata]))
+	for result in gradebook.results.result:
+		if result.score == '100 %':
+			if result.assignment.cdata in stream_mapping:
+				if not result.student.cdata in memberships:
+					memberships[result.student.cdata] = []
 
-getAllUsersURI = config['data']['booked_uri'] + "/Users/"
-r = requests.get(getAllUsersURI, headers=headers)
+				memberships[result.student.cdata].append(int(stream_mapping[result.assignment.cdata]))
 
-for user in r.json()['users']:
-	if user['userName'] in memberships:
-		updateUserURI = config['data']['booked_uri'] + "/Users/" + user['id']
-		user['groups'] = memberships[user['userName']]
-		r = requests.post(updateUserURI, data=json.dumps(user), headers=headers)
+	getAllUsersURI = config['data']['booked_uri'] + "/Users/"
+	r = requests.get(getAllUsersURI, headers=headers)
 
-## Sign out of the session
-signoutURI = config['data']['booked_uri'] + "/Authentication/SignOut"
-signoutData = {'userId':auth['userId'],"sessionToken":auth['sessionToken']}
-r = requests.post(signoutURI, data=json.dumps(signoutData))
+	for user in r.json()['users']:
+		if user['userName'] in memberships:
+			updateUserURI = config['data']['booked_uri'] + "/Users/" + user['id']
+			user['groups'] = memberships[user['userName']]
+			r = requests.post(updateUserURI, data=json.dumps(user), headers=headers)
+
+	## Sign out of the session
+	signoutURI = config['data']['booked_uri'] + "/Authentication/SignOut"
+	signoutData = {'userId':auth['userId'],"sessionToken":auth['sessionToken']}
+	r = requests.post(signoutURI, data=json.dumps(signoutData))
+
+	time.sleep(interval)
